9b0ba40b408842b05a65cfe6408af6ad2f651375
AmalV <amalv@qburst.com>
2017-09-15 14:24:19 +0530

    Adding SAM template for LAmbda

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # codepipeline-crawl
 Crawl through AWS Codepipeline to notify the stages.
+
+
+### How to install 
+1. Clone the repository.
+2. Run `cd codepipeline-crawl` and export the AWS Credentials.
+    ``` export AWS_DEFAULT_REGION="*****"
+        export AWS_ACCESS_KEY_ID=*****
+        export AWS_SECRET_ACCESS_KEY=***** ```
+3. Run `aws cloudformation package --template cloudformation.yaml --s3-bucket test-trail-kamol --output-template-file output-template.yaml`.
+4. Create cloudformation stack with `output-template.yaml`.
--- index.js
+++ index.js
@@ -1,43 +1,48 @@
+'use strict';
 const AWS = require('aws-sdk');
-var codepipeline = new AWS.CodePipeline();
-
 const flaggedStatesString = process.env['FLAGGED_STATES'];
 const slackWebhookUri = process.env['SLACK_URI'];
-var Slack = require('slack-node');
- 
-slack = new Slack();
-slack.setWebhook(slackWebhookUri);
-console.log(slackWebhookUri)
-var flaggedStates = flaggedStatesString ? flaggedStatesString.split(','): ['Failed'];
-codepipeline.listPipelines({}, function (err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else {        // successful response
-    // console.log(pipelines)
-    data.pipelines.forEach(function (pipeline){
-        var params = {
-            name: pipeline.name /* required */
-        };
-        codepipeline.getPipelineState(params, function(err, pipelineListData) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else {
-            pipelineListData.stageStates.forEach(function (stage){
-                if (flaggedStates.indexOf(stage.latestExecution.status)>-1){console.log();
-                    slack.webhook({
-                        attachments:[
-                            {
-                            title: "Failed Pipeline Executions",
-                            color: "#9C1A22",
-                            text: "The Stage: " + stage.stageName + " in Pipeline: " +pipelineListData.pipelineName +" failed.",
-                            }
-                        ]
-                    },function(err, response){
-                        console.log(err)
-                        console.log(response);
-                    });
-                }
-            })
-        }   
-        });
-    })
-  }   
-});
+
+console.log('Loading function');
+exports.handler = (event, context, callback) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    var codepipeline = new AWS.CodePipeline();
+    var Slack = require('slack-node');
+    var slack = new Slack();
+    slack.setWebhook(slackWebhookUri);
+    console.log(slackWebhookUri)
+    var flaggedStates = flaggedStatesString ? flaggedStatesString.split(','): ['Failed'];
+    codepipeline.listPipelines({}, function (err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else {        // successful response
+        // console.log(pipelines)
+        data.pipelines.forEach(function (pipeline){
+            var params = {
+                name: pipeline.name /* required */
+            };
+            codepipeline.getPipelineState(params, function(err, pipelineListData) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else {
+                pipelineListData.stageStates.forEach(function (stage){
+                    if (flaggedStates.indexOf(stage.latestExecution.status)>-1){console.log();
+                        slack.webhook({
+                            attachments:[
+                                {
+                                title: "Failed Pipeline Executions",
+                                color: "#9C1A22",
+                                text: "The Stage: " + stage.stageName + " in Pipeline: " +pipelineListData.pipelineName +" failed.",
+                                }
+                            ]
+                        },function(err, response){
+                            console.log(err)
+                            console.log(response);
+                        });
+                    }
+                })
+            }   
+            });
+        })
+    }   
+    });
+    callback(null, `Successfully processed ${event.Records.length} records.`);
+};
